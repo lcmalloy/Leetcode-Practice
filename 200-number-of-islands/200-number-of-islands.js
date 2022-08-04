32735a2e88756e00bc10067e26ef6c5a9be148e2
Lawrence Malloy <lcmalloy@gmail.com>
2022-08-04 01:47:20 -0700

    Time: 129 ms (54.77%), Space: 49.6 MB (40.57%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,49 +3,41 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let q = [], pairQue = [];
+    let q = [], pair = [];
+    let island = 0;
+    let row = grid.length, column = grid[0].length, first, second;
     
-    let islandCount = 0;
-    
-    let row = grid.length, col = grid[0].length, first, second;
-    
-    for(let i = 0; i<row; i++) {
-        for(let j = 0; j<col; j++) {
-            
-            if (grid[i][j] == '1') {
-                
-                q.push([i,j]);
-                
-                // Visited land
-                grid[i][j] = 'X';
-                
-                while(q.length != 0) {
-                    pairQue = q.shift();
-                
-                    first = pairQue[0];
-                    second = pairQue[1];
-       
-                    if(first-1 >= 0 && grid[first-1][second] == '1') {
-                            q.push([first-1, second]);
-                            grid[first-1][second] = 'X';
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < column; j++) {
+            if (grid[i][j] === '1') {
+                q.push([i, j]);
+                grid[i][j] = 'x';
+                while (q.length !== 0) {
+                    pair =  q.shift();
+                    first = pair[0];
+                    second = pair[1];
+                    
+                    if (first-1 >= 0 && grid[first-1][second] === '1') {
+                        q.push([first-1, second]);
+                        grid[first-1][second] = 'x';
                     }
-                    if(first+1 <= row-1 && grid[first+1][second] == '1') {
-                            q.push([first+1, second]);
-                            grid[first+1][second] = 'X';
+                    if (first+1 <= row-1 && grid[first+1][second] === '1') {
+                        q.push([first+1, second]);
+                        grid[first+1][second] = 'x';
                     }
-                    if(second-1 >=0 && grid[first][second-1] == '1') {
-                            q.push([first, second-1]);
-                            grid[first][second-1] = 'X';
+                    if (second-1 >= 0 && grid[first][second-1] === '1') {
+                        q.push([first, second-1]);
+                        grid[first][second-1] = 'x';
                     }
-                    if(second+1 <= col-1 && grid[first][second+1] == '1') {
-                            q.push([first, second+1]);
-                           grid[first][second+1] = 'X';
+                    if (second+1 <= column-1 && grid[first][second+1] === '1') {
+                        q.push([first, second+1]);
+                        grid[first][second+1] = 'x';
                     }
                 }
                 q.pop();
-                islandCount++;          
-            }   
-        } 
+                island += 1;
+            }
+        }
     }
-    return islandCount;
+    return island;
 };
