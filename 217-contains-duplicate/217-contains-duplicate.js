2ae24dd6285363ce881566f8fdd5b395aeb2f87b
Lawrence Malloy <lcmalloy@gmail.com>
2022-08-03 18:44:04 -0700

    Time: 1242 ms (14.34%), Space: 50.6 MB (68.97%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-
-//1, 2, 3, 1 -> 1, 1, 2, 3
 var containsDuplicate = function(nums) {
     nums.sort((a, b) => a - b);
     
-    for (let i = 1; i < nums.length; i++) { 
-        if (nums[i] === nums[i - 1]) {
-            return true;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return true;
+            }
         }
     }
     return false; 
