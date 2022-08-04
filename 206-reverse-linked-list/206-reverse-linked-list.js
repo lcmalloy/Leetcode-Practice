5771ea4464bda25f30d951f3aefea82d8ab31f5d
Lawrence Malloy <lcmalloy@gmail.com>
2022-08-03 23:33:00 -0700

    Time: 101 ms (42.10%), Space: 44.6 MB (15.35%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let newList = null;
+    
+    while(head) {
+        let next = head.next;
+        head.next = newList;
+        newList = head;
+        head = next;
+    }
+    return newList;
+};
