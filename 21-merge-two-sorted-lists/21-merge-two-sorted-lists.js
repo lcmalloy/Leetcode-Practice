c17995c53e4f064a0a50aa0fc16eb273fbdf71ce
Lawrence Malloy <lcmalloy@gmail.com>
2022-08-04 00:10:24 -0700

    Time: 105 ms (46.89%), Space: 44.3 MB (36.58%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,26 +11,15 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-   const dummy = new ListNode(-Infinity);
-    let prev = dummy;
+    if (!list1) return list2;
+    if (!list2) return list1;
     
-    while(list1 && list2) {
-        if(list1.val <= list2.val) {
-            prev.next = list1;
-            prev = list1;
-            list1 = list1.next;
-        } else {
-            prev.next = list2;
-            prev = list2;
-            list2 = list2.next; 
-        }
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
     
-    if(!list1) {
-        prev.next = list2;
-    } else if (!list2) {
-        prev.next = list1; 
-    }
-    
-    return dummy.next; 
 };
