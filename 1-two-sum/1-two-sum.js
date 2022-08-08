a95f89c7c27e6b3298143964313ac97f562fad31
Lawrence Malloy <lcmalloy@gmail.com>
2022-08-07 19:50:18 -0700

    Time: 117 ms (56.51%), Space: 42.2 MB (81.60%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,14 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let numIndex = [];
-    
-    for (let i = 0; i < nums.length - 1; i++) {
+    for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
-                numIndex = [i , j]
+                return [i, j];
             }
         }
     }
-    return numIndex;
 };
