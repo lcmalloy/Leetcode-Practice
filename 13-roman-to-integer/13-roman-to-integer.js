1cc9ed9beb6b7affd56103e0912946f67c4aee87
Lawrence Malloy <lcmalloy@gmail.com>
2022-08-11 15:47:13 -0700

    Time: 183 ms (69.61%), Space: 47.8 MB (39.00%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -4,80 +4,34 @@
  */
 var romanToInt = function(s) {
     let result = 0;
-    
-    for (let i  = 0; i < s.length; i++) {
-        if (s[i] === 'C') {
-            if (s[i+1] === 'D') {
-                result += 400;
-                i += 1;
-                continue;
-            } else if (s[i+1] === 'M') {
-                result += 900;
-                i += 1;
-                continue;
-            } else {
-                result += 100;
-                continue;
-            }
-        }
-        if (s[i] === 'X') {
-            if (s[i+1] === 'L') {
-                result += 40;
-                i += 1;
-                continue;
-            } else if (s[i+1] === 'C') {
-                result += 90;
-                i += 1;
-                continue;
-            } else {
-                result += 10;
-                continue;
-            }
-        }
-        if (s[i] === 'I') {
-            if (s[i+1] === 'V') {
-                result += 4;
-                i += 1;
-                continue;
-            } else if (s[i+1] === 'X') {
-                result += 9;
-                i += 1;
-                continue;
-            } else if (s[i+1] === 'I') {
-                if (s[i+2] === 'I') {
-                    result += 3;
-                    i += 2;
-                    continue;
-                } else {
-                    result += 2;
-                    i += 1;
-                    continue;
-                }
-            } else {
-                result += 1;
-                continue;
-            }
-        }
-        if (s[i] === 'D') {
-            result += 500;
-        }
-        if (s[i] === 'M') {
-            result += 1000;
-        }
-        if (s[i] === 'L') {
-            result += 50;
-        }
-        if (s[i] === 'C') {
-            result += 100;
-        }
-        if (s[i] === 'V') {
-            result += 5;
-        }
-        if (s[i] === 'X') {
-            result += 10;
-        }
-
+    let rNumeral = {
+        I : 1,
+        V : 5,
+        X : 10,
+        L : 50,
+        C : 100,
+        D : 500,
+        M : 1000,
+        IV : 4,
+        IX : 9,
+        XL : 40,
+        XC : 90,
+        CD : 400,
+        CM : 900
     }
     
+    let i = 0;
+    
+    while (i < s.length) {
+            let doubleSymbol = s.substring(i, i + 2);
+            if (rNumeral[doubleSymbol]) {
+                result += rNumeral[doubleSymbol];
+                i += 2;
+                continue;
+            }
+            let singleSymbol = s.substring(i, i + 1);
+            result += rNumeral[singleSymbol];
+            i += 1;
+        }
     return result;
 };
