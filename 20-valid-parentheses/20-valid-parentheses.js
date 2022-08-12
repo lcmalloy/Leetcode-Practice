fabcb087e5edb45849679958ca25b1557ee53dfa
Lawrence Malloy <lcmalloy@gmail.com>
2022-08-11 20:09:33 -0700

    Time: 81 ms (72.25%), Space: 43.1 MB (17.95%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,34 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack = []; 
-    let string = s.split('');
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] === '{' || string[i] === '(' || string[i] === '['){
-            stack.push(string[i]);                                     
-        } else if (string[i] === '}') {
-            if (stack[stack.length - 1] === '{' ) {
-                stack.pop();                  
-            } else {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i]);
+        }
+        if (s[i] === ')' || s[i] === '}' || s[i] === ']') {
+            let isMatch = stack.pop();
+            if (s[i] === ')' && isMatch !== '(') {
                 return false;
-            }
-        } else if (string[i] === ')') {
-            if (stack[stack.length - 1] === '(') {
-                stack.pop();
-            } else {
+            } else if (s[i] === '}' && isMatch !== '{') {
                 return false;
-            }
-        } else if (string[i] === ']') {
-            if (stack[stack.length - 1] === '[') {
-                stack.pop();
-            } else {
+            } else if (s[i] === ']' && isMatch !== '[') {
                 return false;
             }
         }
     }
     if (stack.length === 0) {
         return true;
-    } else {
-        return false;
     }
+    return false;
 };
