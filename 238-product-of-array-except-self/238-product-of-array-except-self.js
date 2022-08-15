5485861be2341d1af6b73963e08608f41930e49c
Lawrence Malloy <lcmalloy@gmail.com>
2022-08-15 02:12:28 -0700

    Time: 9556 ms (5.01%), Space: 53.3 MB (80.58%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,21 +3,22 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-  let result = [];
+    let result = [];
     
     for (let i = 0; i < nums.length; i++) {
-        let total = 1,
-            left = i - 1,
-            right = i + 1;
-        while (left > -1) {
-            total = total * nums[left];
+        let left = i - 1;
+        let right = i + 1;
+        let sum = 1;
+        
+        while (left >= 0) {
+            sum = sum * nums[left]
             left -= 1;
         }
         while (right < nums.length) {
-            total = total * nums[right];
+            sum = sum * nums[right];
             right += 1;
         }
-        result.push(total);
+        result.push(sum);
     }
     return result;
 };
