32f1c57e95f550f92d0ba49d63425a50fe87b058
Lawrence Malloy <lcmalloy@gmail.com>
2022-09-25 19:26:55 -0700

    Time: 160 ms (77.85%), Space: 50.7 MB (53.42%) - LeetHub

--- 707-design-linked-list/707-design-linked-list.js
+++ 707-design-linked-list/707-design-linked-list.js
@@ -1,73 +1,101 @@
-
 function Node(val) {
-	this.val = val;
-	this.next = null;
+    this.val = val;
+    this.next = null;
 }
 
 var MyLinkedList = function() {
-	this.head = null;
-	this.tail = null;
-	this.size = 0;
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
 };
 
+/** 
+ * @param {number} index
+ * @return {number}
+ */
 MyLinkedList.prototype.get = function(index) {
-	if (index > this.size - 1) return -1;
-	let current = this.head;
-
-	while (index--) current = current?.next;
-	return current?.val ?? -1;
+    if (index > this.size - 1) {
+        return -1;
+    }
+    let current = this.head;
+    while (index--) current = current?.next;
+    return current?.val ?? -1;
 };
 
+/** 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtHead = function(val) {
-	const node = new Node(val);
-
-	if (!this.head) {
-		this.head = this.tail = node;
-	} else {
-		node.next = this.head;
-		this.head = node;
-	}
-	this.size += 1;
+    let node = new Node(val);
+    if (!this.head) {
+        this.head = node;
+        this.tail = node;
+    } else {
+        node.next = this.head;
+        this.head = node;
+    }
+    this.size += 1;
 };
 
+/** 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtTail = function(val) {
-	const node = new Node(val);
-
-	if (!this.head) {
-		this.head = this.tail = node;
-	} else {
-		this.tail.next = node;
-		this.tail = node;
-	}
-	this.size += 1;
+    let node = new Node(val);
+    if (!this.head) {
+        this.head = node;
+        this.tail = node;
+    } else {
+     this.tail.next = node;
+     this.tail = node;
+    }
+    this.size += 1;   
 };
 
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtIndex = function(index, val) {
-	if (index > this.size) return;
-	if (index === 0) return this.addAtHead(val);
-	if (index === this.size) return this.addAtTail(val);
-
-	const node = new Node(val);
-	let current = this.head;
-
-	while (--index) current = current?.next;
-	node.next = current.next;
-	current.next = node;
-	this.size += 1;
+    if (index > this.size) {
+        return;
+    }
+    if (index === this.size) {
+        return this.addAtTail(val);
+    }
+    if (index === 0) {
+        return this.addAtHead(val);
+    }
+    
+    let node = new Node(val);
+    let current = this.head;
+    
+    while (--index) current = current?.next;
+    node.next = current.next;
+    current.next = node;
+    this.size += 1;
 };
 
+/** 
+ * @param {number} index
+ * @return {void}
+ */
 MyLinkedList.prototype.deleteAtIndex = function(index) {
-	if (index >= this.size) return;
-	if (index === 0) {
-		this.head = this.head.next;
-	} else {
-		let current = this.head;
-
-		while (--index) current = current.next;
-		current.next = current.next?.next;
-		if (!current.next) this.tail = current;
-	}
-	this.size -= 1;
+    if (index >= this.size) {
+        return;
+    }
+    if (index === 0) {
+        this.head = this.head.next;
+    } else {
+        let current = this.head;
+        while(--index) current = current.next;
+        current.next = current.next?.next;
+        if (!current.next) this.tail = current;
+    }
+    this.size -= 1;
 };
 
 /** 
