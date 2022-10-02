0a25a97542c732ff89f3c5c285d5fee43d0a417f
Lawrence Malloy <lcmalloy@gmail.com>
2022-10-02 15:23:56 -0700

    Time: 729 ms (34.94%), Space: 87.8 MB (9.88%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    if (nums.length < 2) {
+        return nums;
+    }
+    let mid = Math.floor(nums.length / 2);
+    let left = nums.slice(0, mid);
+    let right = nums.slice(mid);
+    
+    function merge(left, right) {
+        let result = [], lLen = left.length, rLen = right.length, lIndex = 0, rIndex = 0;
+        while (lIndex <lLen && rIndex < rLen) {
+            if (left[lIndex] < right[rIndex]) {
+                result.push(left[lIndex++])
+            } else {
+                result.push(right[rIndex++])
+            }
+        }
+        return result.concat(left.slice(lIndex).concat(right.slice(rIndex)));
+    }
+    return merge(sortArray(left), sortArray(right));
+};
