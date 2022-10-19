007290fd2b90d923bff416b5f3f3e867cb51cd0d
Lawrence Malloy <lcmalloy@gmail.com>
2022-10-19 14:05:21 -0700

    Time: 103 ms (51.90%), Space: 42.1 MB (83.06%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -3,23 +3,9 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length <= 1) {
-        return strs[0];
-    }
-    
-    if (strs.includes("")) {
-        return "";
-    }
-    
-    let right = 0;
-    
-    while (right < strs[0].length) {
-        for (let i = 1; i < strs.length; i++) {
-            if (strs[0][right] !== strs[i][right]) {
-                return strs[0].slice(0, right);
-            }
-        }
-        right += 1;
-    }
-    return strs[0];
+  strs.sort();
+  for (let i = 0; i < strs[0].length; i++) {
+    if (strs[0][i] !== strs[strs.length - 1][i]) return strs[0].substr(0, i);
+  }
+  return strs[0];
 };
