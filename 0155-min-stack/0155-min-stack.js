a7ec33251ce6ed309d4ecc14147a064139017d81
Lawrence Malloy <lcmalloy@gmail.com>
2022-11-02 13:14:23 -0700

    Time: 880 ms (5.07%), Space: 62.9 MB (5.11%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,58 @@
+
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+    this.min = Infinity;
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if (val <= this.min) {
+        this.min = val;
+        this.minStack.push(val);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    let popped = this.stack.pop();
+    if (popped === this.min) {
+        let minPop = this.minStack.pop();
+        if (this.minStack.length > 0) {
+            this.min = this.minStack[this.minStack.length - 1];    
+        } else {
+            this.min = Infinity;
+        }
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    console.log(this.stack);
+    console.log(this.minStack);
+    return this.min;
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
